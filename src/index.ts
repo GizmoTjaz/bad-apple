7a74b66b57070589a7f575cde00a686f4e4adbac
GizmoTjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-04-09 13:04:35 +0200

    implement video downloader into index

--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,104 @@
 // Modules
 import fs from "fs";
-import path from "path";
 import cluster from "cluster";
 
-// Paths
-const tempPath = path.join(__dirname, "../temp");
-const videoPath = path.join(tempPath, "video.mp4");
-
 // Utils
-import { FRAME_PERIOD } from "@utils/constants";
+import { FRAME_PERIOD, TEMP_PATH, VIDEO_PATH } from "@utils/constants";
 
 // Components
 import frameExtractor from "@components/frame_extractor";
 import paintFrame from "@components/paint_frame";
 import drawFrame from "@components/draw_frame";
+import downloadVideo from "@components/download_video";
 
 // Types
 import { WorkerMessageType, Packet, DrawnFrame, RawFrame } from "@typings/types";
 
-// Fix missing directories
-if (!fs.existsSync(tempPath)) fs.mkdirSync(tempPath);
-if (!fs.existsSync(videoPath)) {
+(async () => {
 
-
-
-}
-
-if (cluster.isMaster) {
-
-	const worker = cluster.fork();
-	const framePackets: Packet[] = [];
-
-	worker.on("message", ({ type, data }: { type: WorkerMessageType, data: string | Packet }) => {
-		switch (type) {
-			case "message":
-				console.log(data);
-				break;
-			case "packet":
-				framePackets.push(data as Packet);
-				break;
-			default:
-		}
-	});
-
-	function unpackPacket (packet: Packet) {
-		packet.forEach((frame: DrawnFrame, index) => {
-			setTimeout(() => {
-				paintFrame(frame);
-				if (index === (packet.length - 1)) fetchNewPacket();
-			}, index * FRAME_PERIOD);
-		});
+	// Fix missing directories
+	if (!fs.existsSync(TEMP_PATH)) fs.mkdirSync(TEMP_PATH);
+	if (!fs.existsSync(VIDEO_PATH)) {
+		await downloadVideo();
 	}
 
-	function fetchNewPacket () {
-
-		const framePacket: Packet = framePackets[0];
-
-		if (framePackets) {
-			framePackets.shift();
-			unpackPacket(framePacket);
-		} else {
-			setTimeout(() => {
-				fetchNewPacket();
-			}, FRAME_PERIOD);
-		}
-
+	for (let i = 0; i < 36; i++) {
+		process.stdout.write("\n");
 	}
 
-	fetchNewPacket();
+	if (cluster.isMaster) {
 
-} else {
+		const worker = cluster.fork();
+		const framePackets: Packet[] = [];
 
-	let rawFrameQueue: RawFrame[] = [];
-
-	setInterval(async () => {
-		if (rawFrameQueue.length > 10) {
-
-			const rawPacketFrames = rawFrameQueue.slice(0, 10);
-			rawFrameQueue = rawFrameQueue.slice(10);
-
-			const framePacket = new Array(rawPacketFrames.length);
-
-			for await (const [ packetFrameIndex ] of framePacket.entries()) {{
-				framePacket[packetFrameIndex] = await drawFrame(rawPacketFrames[packetFrameIndex]);
-			}}
-
-			if (process) {
-				process.send!({ type: "packet", data: framePacket });
+		worker.on("message", ({ type, data }: { type: WorkerMessageType, data: string | Packet }) => {
+			switch (type) {
+				case "message":
+					console.log(data);
+					break;
+				case "packet":
+					framePackets.push(data as Packet);
+					break;
+				default:
 			}
+		});
+
+		function unpackPacket (packet: Packet) {
+			packet.forEach((frame: DrawnFrame, index) => {
+				setTimeout(() => {
+					paintFrame(frame);
+					if (index === (packet.length - 1)) fetchNewPacket();
+				}, index * FRAME_PERIOD);
+			});
 		}
-	}, FRAME_PERIOD);
 
-	frameExtractor(videoPath, (frame: RawFrame) => {
-		rawFrameQueue.push(frame);
-	}).then(() => {
-		process.exit(0);
-	}).catch(err => {
-		throw err;
-	});
+		function fetchNewPacket () {
 
-}
+			const framePacket: Packet = framePackets[0];
+
+			if (framePacket) {
+				framePackets.shift();
+				unpackPacket(framePacket);
+			} else {
+				setTimeout(() => {
+					fetchNewPacket();
+				}, FRAME_PERIOD);
+			}
+
+		}
+
+		fetchNewPacket();
+
+	} else {
+
+		let rawFrameQueue: RawFrame[] = [];
+
+		setInterval(async () => {
+			if (rawFrameQueue.length > 10) {
+
+				const rawPacketFrames = rawFrameQueue.slice(0, 10);
+				rawFrameQueue = rawFrameQueue.slice(10);
+
+				const framePacket = new Array(rawPacketFrames.length);
+
+				for await (const [ packetFrameIndex ] of framePacket.entries()) {{
+					framePacket[packetFrameIndex] = await drawFrame(rawPacketFrames[packetFrameIndex]);
+				}}
+
+				if (process) {
+					process.send!({ type: "packet", data: framePacket });
+				}
+			}
+		}, FRAME_PERIOD);
+
+		frameExtractor(VIDEO_PATH, (frame: RawFrame) => {
+			rawFrameQueue.push(frame);
+		}).then(() => {
+			process.exit(0);
+		}).catch(err => {
+			throw err;
+		});
+
+	}
+
+})();
