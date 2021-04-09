89f05c0e698da56c054bec399b112a410bf34410
GizmoTjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-04-09 13:04:05 +0200

    clear screen before painting frame

--- src/components/paint_frame.ts
+++ src/components/paint_frame.ts
@@ -2,6 +2,11 @@
 import { DrawnFrame } from "@typings/types";
 
 export default function (drawnFrame: DrawnFrame) {
-	process.stdout.cursorTo(0);
+	//process.stdout.cursorTo(0);
+	
+	for (let i = 0; i < 36; i++) {
+		process.stdout.write("\x1B[F\x1B[2K");
+	}
+
 	process.stdout.write(drawnFrame.data);
 }
