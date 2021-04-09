db714443b0abac8ab018df23dc427ab9f36c34fe
GizmoTjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-04-09 13:04:21 +0200

    TEMP_PATH & VIDEO_PATH constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,7 @@
+// Modules
+import path from "path";
+
 export const PLAYBACK_FPS = 30;
 export const FRAME_PERIOD = 1 / 30 * 1000;
+export const TEMP_PATH = path.join(__dirname, "../", "temp");
+export const VIDEO_PATH = path.join(TEMP_PATH, "video.mp4");
