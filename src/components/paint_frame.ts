94ef308a9ee4680768779a19bc98c32ae8b23c34
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2021-04-29 16:20:28 +0200

    properly paint frames

--- src/components/paint_frame.ts
+++ src/components/paint_frame.ts
@@ -2,11 +2,19 @@
 import { DrawnFrame } from "@typings/types";
 
 export default function (drawnFrame: DrawnFrame) {
+
 	//process.stdout.cursorTo(0);
 	
-	for (let i = 0; i < 36; i++) {
-		process.stdout.write("\x1B[F\x1B[2K");
-	}
+	//for (let i = 0; i < 36; i++) {
+	//	process.stdout.write("\x1B[F\x1B[2K");
+	//}
 
-	process.stdout.write(drawnFrame.data);
+	// console.log(drawnFrame);
+	// a = !a;
+
+	//process.stdout.write(new Array(36).fill("\x1B[1A").join("\x1B["))
+
+	//process.stdout.write("\x1B[36A" + `${ a ? "\x1B[94m" : "\x1B[31m" }` + drawnFrame.data);
+
+	process.stdout.write("\x1B[2J" + drawnFrame.data);
 }
