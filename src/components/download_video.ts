27011015fc19a06a61838cc3b0f186bf88fb9179
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2023-02-02 17:52:25 +0100

    set default video URL

--- src/components/download_video.ts
+++ src/components/download_video.ts
@@ -7,49 +7,49 @@ import readline from "readline";
 import { VIDEO_PATH } from "@utils/constants";
 import loadingBuffer from "@utils/loading_buffer";
 
-// function askForVideoURL (callback: (url: string) => void): void {
+function askForVideoURL (callback: (url: string) => void): void {
 
-// 	const rl = readline.createInterface({
-// 		input: process.stdin,
-// 		output: process.stdout
-// 	});
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
 
-// 	rl.question("YouTube video URL (default: Bad Apple): ", (url: string) => {
-// 		if (url) {
-// 			callback(url);
-// 		} else {
-// 			console.error("You must specify a URL");
-// 			askForVideoURL(callback);
-// 		}
-// 	});
+	process.stdout.write("== First Time Setup ==\n");
 
-// }
+	rl.question("Video URL (default: YouTube): ", (url: string) => {
+		if (url.trim().length > 0) {
+			callback(url.trim());
+		} else {
+			callback("https://youtu.be/FtutLA63Cp8");
+		}
+	});
+
+}
 
 export default function downloadVideo (): Promise<void> {
 	return new Promise((res, rej): void => {
+		askForVideoURL((url: string) => {
 
-		const video = ytdl("https://youtu.be/FtutLA63Cp8", {
-			quality: "highest"
+			const video = ytdl(url, {
+				quality: "highest"
+			});
+
+			video.pipe(fs.createWriteStream(VIDEO_PATH));
+
+			video.on("data", () => {
+				console.log("\x1B[F" + `[${loadingBuffer()}] Downloading Video...`);
+			});
+
+			video.on("error", rej);
+
+			video.on("end", () => {
+				console.log("\x1B[F\x1B[0K[#] Downloaded Video\n");
+				res();
+			});
+			
+			// Go to new line so it doesn't overwrite input prompts
+			process.stdout.write("\n");
+
 		});
-
-		video.pipe(fs.createWriteStream(VIDEO_PATH));
-
-		video.on("data", () => {
-			console.log("\x1B[F" + `[${loadingBuffer()}] Downloading Video...`);
-		});
-
-		video.on("end", () => {
-			console.log("\x1B[F\x1B[0K[#] Downloaded Video\n");
-			res();
-		});
-
-		video.on("error", rej);
-
-		// askForVideoURL((url: string) => {
-
-		// 	// Go to new line so it doesn't overwrite input prompts
-		// 	process.stdout.write("\n");
-
-		// });
 	});
 }
