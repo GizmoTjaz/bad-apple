79adad89e2d07de7406dfbd70158a26ed247fa57
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2023-02-02 17:52:58 +0100

    use `PLAYBACK_FPS`

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,6 @@
 import path from "path";
 
 export const PLAYBACK_FPS = 30;
-export const FRAME_PERIOD = 1 / 30 * 1000;
+export const FRAME_PERIOD = 1 / PLAYBACK_FPS * 1000;
 export const TEMP_PATH = path.join(__dirname, "../", "temp");
 export const VIDEO_PATH = path.join(TEMP_PATH, "video.mp4");
